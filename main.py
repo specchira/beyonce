23a752832acd53417c442fc288027a836401a0d1
corsamove <kim.maksim24072709@mail.ru>
2023-04-11 22:22:52 +0300

    Добавил файл командной решенной задачи

---
+++ main.py
@@ -0,0 +1,16 @@
+def combsort(num):
+    gap = len(num)
+    swaps = True
+    while gap > 1 or swaps:
+        gap = max(1, int(gap / 1.3)) 
+        swaps = False
+        for i in range(len(num) - gap):
+            j = i+gap
+            if num[i] > num[j]:
+                num[i], num[j] = num[j], num[i]
+                swaps = True
+
+num = [3, 14, 1, 7, 9, 8, 11, 6, 4, 2]
+print("Before: ", num)
+combsort(num)
+print("After:  ", num) 
